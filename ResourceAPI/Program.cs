8fdf0aa4616e519790200b6e345c9b4630ca2a23
jamescaldwell82 <james.caldwell82@outlook.com>
2022-05-31 16:21:11 -0500

    Added CORS steps...all steps now added

--- ResourceAPI/Program.cs
+++ ResourceAPI/Program.cs
@@ -8,6 +8,15 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+//Step 11. Add Cors functionality to determine what websites can access the data in this application
+//CORS stands for Cross Origin Resource Sharing and by default browsers use this to block websites from requesting data unless that website has permission to do so. This code below determines what websites have access to CORS with this API.
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins("OriginPolicy", "http://localhost:3000");
+    });
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
